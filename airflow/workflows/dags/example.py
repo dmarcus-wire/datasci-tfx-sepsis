91fc4edc9e790128b3937e13338e23c5fd9fb854
Noah Sayre <nsayre@redhat.com>
2022-04-29 13:49:00 -0400

    change example dag to use airflow namespace

--- airflow/workflows/dags/example.py
+++ airflow/workflows/dags/example.py
@@ -23,7 +23,7 @@ dag = DAG(
 start = DummyOperator(task_id='run_this_first', dag=dag)
 
 passing = KubernetesPodOperator(
-    namespace='sepsis-detection',
+    namespace='airflow',
     image="python:3.6",
     cmds=["python","-c"],
     arguments=["print('hello world')"],
