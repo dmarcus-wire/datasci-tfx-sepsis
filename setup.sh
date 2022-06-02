723258b2ea2b323c895220e6fbb584f106f3c203
1000670000 user <dmarcus@redhat.com>
2022-06-02 17:41:45 +0000

    setup script updated

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ NORMAL=$(tput sgr0)
 
 # create directories {code, data, models, notebooks, pipeline}
 printf "${GREEN}Creating folder structure${NORMAL}\n\n"
-mkdir {code,data,models,notebooks,pipeline}
-mkdir data/{attributes,raw}
+mkdir {code,data,models,notebooks,pipeline} 2>&1 >/dev/null
+mkdir data/{attributes,raw} 2>&1 >/dev/null
 
 # create __init__.py in code dir
 printf "${GREEN}Creating __init.py in code dir${NORMAL}\n\n"
@@ -16,8 +16,9 @@ touch code/__init__.py
 
 # install requirements.txt packages
 printf "${GREEN}Install requirements.txt${NORMAL}\n\n"
-pip install -r requirements.txt
+pip install -r requirements.txt 2>&1 >/dev/null
 
 # cleanup notebook checkpoint files that cause errors "RuntimeError: Files in same split /* have different header."
-printf "{GREEN}Cleanup notebook checkpoint files in data directories.${NORMAL}\n\n"
-rm -rf data/{new,raw}/.ipynb_checkpoints
+printf "${GREEN}Cleanup notebook checkpoint files in data directories.${NORMAL}\n\n"
+rm -rf data/{new,raw}/.ipynb_checkpoints 2>&1 >/dev/null
+
